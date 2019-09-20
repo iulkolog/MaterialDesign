b9555bbeaf837d0ac4734b2fe3623fd5bdb048d7
iulkolog <iulkolog@gmail.com>
2019-09-20 16:50:52 +0300

    lesson 4

---
+++ MDL4/app/src/main/java/yuliya/loginova/ru/mdl4/ToolbarActivity.java
@@ -0,0 +1,40 @@
+package yuliya.loginova.ru.mdl4;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuItem;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    private static final String TAG = "ToolbarActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_toolbar);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setLogo(R.mipmap.ic_launcher);
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        switch (id) {
+            case android.R.id.home:
+                Log.d(TAG, "onOptionsItemSelected: ");
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
